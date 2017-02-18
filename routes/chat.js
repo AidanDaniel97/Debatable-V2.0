0274a31eda33d8406ad1ea135e146522fc230726
Aidan Daniel <aidan.d@wearedrum.com>
2017-02-18 15:32:58 +0000

    Now have to be logged in to debate, changed some layouts

--- routes/chat.js
+++ routes/chat.js
@@ -3,8 +3,9 @@ var mongodb = require('mongodb');
 var date = require('./controllers/date_time')
 var router = express.Router();
 
+
 // Get admin page /chat
-router.get("/:debate_name/:debate_mode", function(req,res){
+router.get("/:debate_name/join", ensureAuthenticated, function(req,res){
 	//get debate information from databse here 
 	var MongoClient = mongodb.MongoClient;
 	var mongoUrl = "mongodb://127.0.0.1:27017/debatable";
@@ -18,12 +19,12 @@ router.get("/:debate_name/:debate_mode", function(req,res){
 			var topics = db.collection("topics"); 
 			topics.find({"topic_name_short" : decodeURI(req.params.debate_name)}).toArray(function(err,result){ 
 				if (err){
-					console.log("Error retrieving database collection");
+					console.log("Error retrieving database collection"); 
 				}else if (result){ 
-					res.render("chat", {'debate_mode':req.params.debate_mode, 'debate_db_info':result}) //Load the view chat  
+					res.render("chat", {'debate_mode':'join', 'debate_db_info':result, title: 'my other page', layout: 'chat_layout'}) //Load the view chat  
 				}else{
 					console.log("No data found in collection"); 
-					res.render("chat", {'debate_mode':req.params.debate_mode, 'debate_db_info':result}) //Load the view chat 
+					res.render("chat", {'debate_mode':'join', 'debate_db_info':result, title: 'my other page', layout: 'chat_layout'}) //Load the view chat 
 				}
 
 				db.close();
@@ -37,5 +38,52 @@ router.get("/:debate_name/:debate_mode", function(req,res){
 
 
 });
- 
+
+
+
+
+// Get admin page /chat
+router.get("/:debate_name/spectate", function(req,res){
+	//get debate information from databse here 
+	var MongoClient = mongodb.MongoClient;
+	var mongoUrl = "mongodb://127.0.0.1:27017/debatable";
+
+	MongoClient.connect(mongoUrl,function(err,db){
+		if(err){
+			console.log("Unable to connect to MongoDB " , err)
+		}else{
+			console.log("Connection establish with MongoDB")
+		
+			var topics = db.collection("topics"); 
+			topics.find({"topic_name_short" : decodeURI(req.params.debate_name)}).toArray(function(err,result){ 
+				if (err){
+					console.log("Error retrieving database collection"); 
+				}else if (result){ 
+					res.render("chat", {'debate_mode':'spectate', 'debate_db_info':result, title: 'my other page', layout: 'chat_layout'}) //Load the view chat  
+				}else{
+					console.log("No data found in collection"); 
+					res.render("chat", {'debate_mode':'spectate', 'debate_db_info':result, title: 'my other page', layout: 'chat_layout'}) //Load the view chat 
+				}
+
+				db.close();
+			});
+
+			
+		}
+
+
+	})
+
+
+});
+
+function ensureAuthenticated(req, res, next){
+	if(req.isAuthenticated()){
+		return next();
+	} else {
+		//req.flash('error_msg','You are not logged in');
+		res.redirect('/');
+	}
+}
+
 module.exports = router;
--- routes/index.js
+++ routes/index.js
@@ -4,42 +4,49 @@ var date = require('./controllers/date_time')
 var router = express.Router();
 
 // Get Homepage
-router.get('/', ensureAuthenticated, function(req, res){
+router.get('/',function(req, res){
 	
-
 		console.log("Requested Homepage")
 		var MongoClient = mongodb.MongoClient;
 		var mongoUrl = "mongodb://127.0.0.1:27017/debatable";
 		var server_date = date.get_date();
 
 
-	MongoClient.connect(mongoUrl,function(err,db){
-		if(err){
-			console.log("Unable to connect to MongoDB " , err)
-		}else{
-			console.log("Connection establish with MongoDB")
-		
-			var topics = db.collection("topics");
 
-			topics.find().toArray(function(err,result){ 
-				if (err){
-					console.log("Error retrieving database collection");
-				}else if (result){ 
-					res.render("index",{'topic_list':result,'current_date':server_date,"username":req.user.name,"admin":req.user.admin}); //the response data can be changed
-				}else{
-					console.log("No data found in collection");
-					res.render("index",{'topic_list':result,'current_date':server_date,"username":req.user.name,"admin":req.user.admin}); //the response data can be changed
-				}
-
-				db.close();
-			});
-
+		MongoClient.connect(mongoUrl,function(err,db){
+			if(err){
+				console.log("Unable to connect to MongoDB " , err)
+			}else{
+				console.log("Connection establish with MongoDB")
 			
-		}
+				var topics = db.collection("topics");
+
+				topics.find().toArray(function(err,result){ 
+					if (err){
+						console.log("Error retrieving database collection");
+					}else if (result){ 
+						if(req.isAuthenticated()){
+							res.render("index",{'topic_list':result,'current_date':server_date,"authenticated":true,"username":req.user.name,"admin":req.user.admin}); //the response data can be changed
+							
+						}else{
+							res.render("index",{'topic_list':result,'current_date':server_date}); //the response data can be changed
+							
+						}
+
+					}else{
+						console.log("No data found in collection");
+						res.render("index",{'topic_list':result,'current_date':server_date,"username":req.user.name,"admin":req.user.admin}); //the response data can be changed
+					}
+
+					db.close();
+				});
+
+				
+			}
 
 
-	})
- 
+		})
+	 
 
 
 
@@ -53,6 +60,6 @@ function ensureAuthenticated(req, res, next){
 		//req.flash('error_msg','You are not logged in');
 		res.redirect('/users/login');
 	}
-}
+}//
 
 module.exports = router;
